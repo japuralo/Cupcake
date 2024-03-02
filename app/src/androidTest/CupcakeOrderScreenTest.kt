208fa4c8e833598afa5fbf8a775df3779c6f4b1f
Javier Alejandro Puras Lombillo <javijandro99@gmail.com>
2024-03-02 14:55:29 +0100

    Paso 3.6

---
+++ app/src/androidTest/CupcakeOrderScreenTest.kt
@@ -0,0 +1,29 @@
+class CupcakeOrderScreenTest
+{
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun selectOptionScreen_verifyContent()
+    {
+        val flavors = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+        val subtotal = "$100"
+
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal = subtotal, options = flavors)
+        }
+
+        flavors.forEach { flavor ->
+            composeTestRule.onNodeWithText(flavor).assertIsDisplayed()
+        }
+
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.subtotal_price,
+                subtotal
+            )
+        ).assertIsDisplayed()
+
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsNotEnabled()
+    }
+}
